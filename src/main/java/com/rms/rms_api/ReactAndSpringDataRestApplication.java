8e345d9dc59fa26c2b4e35b72bbe85b714f8e733
BagasAjah <BagasDimas.Permadi@mitrais.com>
2017-04-04 15:08:59 +0700

    innitial commit database migration

--- src/main/java/com/rms/rms_api/ReactAndSpringDataRestApplication.java
+++ src/main/java/com/rms/rms_api/ReactAndSpringDataRestApplication.java
@@ -1,5 +1,9 @@
 package com.rms.rms_api;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.flywaydb.core.Flyway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +11,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ReactAndSpringDataRestApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws URISyntaxException {// Start migration before execution
+		URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+        Flyway flyway = new Flyway();
+        flyway.setDataSource(dbUrl, username, password);
+        flyway.migrate();
 		SpringApplication.run(ReactAndSpringDataRestApplication.class, args);
+		
 	}
 }
