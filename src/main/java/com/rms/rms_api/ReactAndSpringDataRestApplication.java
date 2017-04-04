d5d75a84f9f832c74a4232d460423a46fec7144a
BagasAjah <BagasDimas.Permadi@mitrais.com>
2017-04-04 15:31:12 +0700

    update database migration fix

--- src/main/java/com/rms/rms_api/ReactAndSpringDataRestApplication.java
+++ src/main/java/com/rms/rms_api/ReactAndSpringDataRestApplication.java
@@ -1,8 +1,5 @@
 package com.rms.rms_api;
 
-import java.net.URI;
-import java.net.URISyntaxException;
-
 import org.flywaydb.core.Flyway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,14 +8,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ReactAndSpringDataRestApplication {
 
-	public static void main(String[] args) throws URISyntaxException {// Start migration before execution
-		URI dbUri = new URI(System.getenv("DATABASE_URL"));
-
-        String username = dbUri.getUserInfo().split(":")[0];
-        String password = dbUri.getUserInfo().split(":")[1];
-        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
-        Flyway flyway = new Flyway();
-        flyway.setDataSource(dbUrl, username, password);
+	public static void main(String[] args) {// Start migration before execution
+		Flyway flyway = new Flyway();
+        flyway.setDataSource(System.getenv("JDBC_DATABASE_URL"),
+                             System.getenv("JDBC_DATABASE_USERNAME"),
+                             System.getenv("JDBC_DATABASE_PASSWORD"));
         flyway.migrate();
 		SpringApplication.run(ReactAndSpringDataRestApplication.class, args);
 		
