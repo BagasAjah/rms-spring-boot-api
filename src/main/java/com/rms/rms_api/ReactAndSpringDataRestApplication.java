f00a445239eb8a2c7b2e07dd2741f9d8205f9614
BagasAjah <BagasDimas.Permadi@mitrais.com>
2017-04-06 08:28:14 +0700

    Revert "Merge pull request #3 from BagasAjah/dev"
    This reverts commit d29fc6892ccf6eb03731f94a323cbad789938001, reversing
    changes made to 81de8e901821b6f04b261c020737d49bda2b74ae.

--- src/main/java/com/rms/rms_api/ReactAndSpringDataRestApplication.java
+++ src/main/java/com/rms/rms_api/ReactAndSpringDataRestApplication.java
@@ -1,6 +1,5 @@
 package com.rms.rms_api;
 
-import org.flywaydb.core.Flyway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,13 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ReactAndSpringDataRestApplication {
 
-	public static void main(String[] args) {// Start migration before execution
-		Flyway flyway = new Flyway();
-        flyway.setDataSource(System.getenv("JDBC_DATABASE_URL"),
-                             System.getenv("JDBC_DATABASE_USERNAME"),
-                             System.getenv("JDBC_DATABASE_PASSWORD"));
-        flyway.migrate();
+	public static void main(String[] args) {
 		SpringApplication.run(ReactAndSpringDataRestApplication.class, args);
-		
 	}
 }
